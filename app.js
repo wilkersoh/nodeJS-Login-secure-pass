4594989ca4aa2a89265afb17197229a2f6c9e108
wilkersoh <plgu@hotmail.com>
2019-05-16 15:24:33 +0800

    app

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const ejsLayout = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session')
+const passport = require('passport')
 
 
 const app = express();
+
+// Passport 的 资料
+require('./config/passport')(passport)
 
 // mongoose
 mongoose.connect(
@@ -23,21 +27,25 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-// session 中間件 和 flash 組合
+// session 中間件 和 flash 組合 * 必須在flash中間件上面
 app.use(session({
     // copy from github express-session
     secret: 'secret',
     resave: false,
     saveUninitialized: true,
 }))
+//  copy from passport documentary middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // 提醒已經做好賬號了 和setTimeout 一樣
 app.use(flash())
-// 全局 var 都能用
+// 全局  都能用这个flash
 app.use((req, res, next) => {
     // flash(ejs 現實的信息)
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 })
 
@@ -47,3 +55,4 @@ app.use('/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Listening PORT ${PORT} Now.`));
+
